304bbc6eb8ce922c4e85aaa0f296061eda7329ca
Claudio Neves <cfneguacu@gmail.com>
2026-04-15 22:16:44 -0300

    first commit

---
+++ sorts.py
@@ -0,0 +1,63 @@
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+def cocktail_sort(arr):
+    n = len(arr)
+    swapped = True
+    start = 0
+    end = n - 1
+
+    while swapped:
+        swapped = False
+
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        end -= 1
+
+        for i in range(end, start - 1, -1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        start += 1
+
+    return arr
+
+if __name__ == "__main__":
+    sample_array = [38, 27, 43, 3, 9, 82, 10]
+    print("Original array:", sample_array)
+    sorted_array = merge_sort(sample_array)
+    print("Sorted array (Merge Sort):", sorted_array)
+    sorted_array = cocktail_sort(sample_array)
+    print("Sorted array (Cocktail Sort):", sorted_array)
