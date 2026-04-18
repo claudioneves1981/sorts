7b11dab049a1f33830440e3f66c6227d3ce7c98e
Claudio Neves <cfneguacu@gmail.com>
2026-04-17 21:53:22 -0300

    Incluindo Heap Sort

--- sorts.py
+++ sorts.py
@@ -56,6 +56,32 @@ def array_comparision_swap(arr, i):
      if arr[i] > arr[i + 1]:
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
        
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
 
 
 if __name__ == "__main__":
@@ -65,3 +91,5 @@ if __name__ == "__main__":
     print("Sorted array (Merge Sort):", sorted_array)
     sorted_array = cocktail_sort(sample_array)
     print("Sorted array (Cocktail Sort):", sorted_array)
+    sorted_array = heapSort(sample_array)
+    print("Sorted array (Heap Sort):", sorted_array)
